f7fab7bf4dbd9ec07798b1c94671c3442684559e
Marco Cardoso <marcoacardosom@gmail.com>
2022-06-13 22:39:22 -0500

    src: define training loop

--- paper/src/bci.py
+++ paper/src/bci.py
@@ -20,26 +20,31 @@ import torch.optim as optim
 class bci_cnn(nn.Module):
     """
     Definition of the convolutional neural network used for SSVEP-BCI systems.
+
+    Loss function at page 187!!!
     """
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(5, 2, kernel_size=1, padding=1)
         self.act1 = nn.Tanh()
-        self.pool1 = nn.MaxPool2d(2)
-        self.conv2 = nn.Conv2d(16, 8, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(2, 1, kernel_size=1, padding=1)
         self.act2 = nn.Tanh()
-        self.pool2 = nn.MaxPool2d(2)
-        self.fc1 = nn.Linear(8 * 8 * 8, 32)
-        self.act3 = nn.Tanh()
-        self.fc2 = nn.Linear(32, 2)
+        # self.conv2 = nn.Conv2d(16, 8, kernel_size=3, padding=1)
+        # self.pool2 = nn.MaxPool2d(2)
+        # self.fc1 = nn.Linear(8 * 8 * 8, 32)
+        # self.act3 = nn.Tanh()
+        # self.fc2 = nn.Linear(32, 2)
 
 
     def forward(self, x):
-        out = self.pool1(self.act1(self.conv1(x)))
-        out = self.pool2(self.act2(self.conv2(out)))
-        out = out.view(-1, 8 * 8 * 8)
-        out = self.act3(self.fc1(out))
-        out = self.fc2(out)
+        out = self.act1(self.conv1(x))
+        print(out)
+        out = self.act2(self.conv2(x))
+        # out = self.pool1(self.act1(self.conv1(x)))
+        # out = self.pool2(self.act2(self.conv2(out)))
+        # out = out.view(-1, 8 * 8 * 8)
+        # out = self.act3(self.fc1(out))
+        # out = self.fc2(out)
         return out
 
 
@@ -62,8 +67,19 @@ class beta_dataset(torch.utils.data.Dataset):
             shape:
             - number of tests in bci (same as 4th dimension in signals)
         """
-        self.eeg_signals = eeg_signals
-        self.labels = labels
+        if isinstance(eeg_signals, np.ndarray):
+            self.eeg_signals = torch.from_numpy(eeg_signals)
+        elif isinstance(eeg_signals, list):
+            self.eeg_signals = torch.tensor(eeg_signals)
+        else:
+            self.eeg_signals = torch.tensor(eeg_signals)
+
+        if isinstance(labels, np.ndarray):
+            self.labels = torch.from_numpy(labels)
+        elif isinstance(labels, list):
+            self.labels = torch.tensor(labels)
+        else:
+            self.labels = torch.tensor(labels)
 
 
     def __len__(self):
@@ -76,3 +92,24 @@ class beta_dataset(torch.utils.data.Dataset):
             idx = idx.tolist()
 
         return (self.eeg_signals[:, :, :, idx], self.labels[idx])
+
+
+def training_loop(n_epochs, optimizer, model, loss_fn, train_loader, device):
+    for epoch in range(1, n_epochs + 1):
+        loss_train = 0.0
+        for signals, labels in train_loader:
+            signals = signals.to(device=device)  # <1>
+            labels = labels.to(device=device)
+            outputs = model(signals)
+            loss = loss_fn(outputs, labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            loss_train += loss.item()
+
+        if epoch == 1 or epoch % 10 == 0:
+            print('{} Epoch {}, Training loss {}'.format(
+                datetime.datetime.now(), epoch,
+                loss_train / len(train_loader)))
