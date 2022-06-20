1ec5f023f01d8a53bbb810ceb1e8b79dadd56704
Marco Cardoso <marcoacardosom@gmail.com>
2022-06-19 21:02:43 -0500

    bci: refactored bci dataset and new NN arch
    Refactored the bci dataset to do all operations (loading from file,
    storing in an array, etc) inside its own __init__ method
    
    New neural network architecture that takes as number of channels for
    convolution the EEG channels selected

--- src/bci.py
+++ src/bci.py
@@ -14,8 +14,8 @@ References
 import numpy as np
 import torch
 import torch.nn as nn
-# import torch.nn.functional as F
-# import torch.optim as optim
+import torch.nn.functional as F
+import torch.optim as optim
 
 torch.set_printoptions(precision=3)
 
@@ -31,114 +31,231 @@ class bci_cnn(nn.Module):
         super().__init__()
 
         # filter bank
-        self.conv1 = nn.Conv2d(5, 6, kernel_size=1, padding="same").double()
-        self.norm1 = nn.BatchNorm2d(6).double()
+        self.conv1 = nn.Conv2d(5, 10, kernel_size=(1, 5), padding="same").double()
         self.act1 = nn.Tanh()
+        self.norm1 = nn.BatchNorm2d(10).double()
 
-        self.conv2 = nn.Conv2d(6, 1, kernel_size=(1, 3),
+        self.conv2 = nn.Conv2d(10, 5, kernel_size=(1, 5),
                                padding="same").double()
-        self.norm2 = nn.BatchNorm2d(1).double()
         self.act2 = nn.Tanh()
+        self.norm2 = nn.BatchNorm2d(5).double()
+
+        self.conv2_1 = nn.Conv2d(5, 1, kernel_size=(1, 5),
+                               padding="same").double()
+        self.act2_1 = nn.ReLU()
+        self.norm2_1 = nn.BatchNorm2d(1).double()
 
         # channel selection
-        self.conv3 = nn.Conv2d(1, 10, kernel_size=(5, 1), padding="valid",
+        self.conv3 = nn.Conv2d(1, 1, kernel_size=(5, 1), padding="valid",
                                stride=(4, 1)).double()
-        self.norm3 = nn.BatchNorm2d(10).double()
         self.act3 = nn.Tanh()
+        self.norm3 = nn.BatchNorm2d(1).double()
 
-        self.conv4 = nn.Conv2d(10, 1, kernel_size=(2, 3), padding=(0, 1),
+        self.conv4 = nn.Conv2d(1, 1, kernel_size=(2, 5), padding=(0, 2),
                                stride=(1, 1)).double()
-        self.norm4 = nn.BatchNorm2d(1).double()
         self.act4 = nn.Tanh()
+        self.norm4 = nn.BatchNorm2d(1).double()
         self.pool4 = nn.MaxPool2d(kernel_size=(1, 2))
 
-        # self.conv5 = nn.Conv2d(1, 10, kernel_size=(1, 3),
-        #                        padding="same").double()
-        # self.norm5 = nn.BatchNorm2d(10).double()
-        # self.act5 = nn.Tanh()
-        # self.pool5 = nn.MaxPool2d(kernel_size=(1, 2))
+        self.conv5 = nn.Conv2d(1, 10, kernel_size=(1, 5),
+                               padding="same").double()
+        self.act5 = nn.Tanh()
+        self.norm5 = nn.BatchNorm2d(10).double()
+        self.pool5 = nn.MaxPool2d(kernel_size=(1, 2))
+        self.drop5 = nn.Dropout(p=0.9)
 
-        self.linear6 = nn.Linear(1 * 1 * 125, 40).double()
+        self.linear6 = nn.Linear(620, 40).double()
 
     def forward(self, x):
         """Convolutional Neural Network forward pass."""
+        batch_size = x.shape[0]
+
         # filterbank
-        # print(f"shape of input: {x.shape}")
         out = self.conv1(x)
-        out = self.norm1(out)
+        # out = self.norm1(out)
         out = self.act1(out)
         # print(f"shape of output after layer 1: {out.shape}")
 
         # second stage of filter bank
         out = self.conv2(out)
-        out = self.norm2(out)
         out = self.act2(out)
+        out = self.norm2(out)
+
+        out = self.conv2_1(out)
+        out = self.act2_1(out)
+        out = self.norm2_1(out)
         # out = self.pool2(out)
         # out = self.drop2(out)
         # print(f"shape of output after layer 2: {out.shape}")
 
         # channel selection
         out = self.conv3(out)
-        out = self.norm3(out)
         out = self.act3(out)
+        ut = self.norm3(out)
         # print(f"shape of out after layer 3: {out.shape}")
 
         out = self.conv4(out)
-        out = self.norm4(out)
         out = self.act4(out)
+        out = self.norm4(out)
         out = self.pool4(out)
         # print(f"shape of out after layer 4: {out.shape}")
 
-        # out = self.conv5(out)
-        # out = self.norm5(out)
-        # out = self.act5(out)
-        # out = self.pool5(out)
-        # exit()
+        out = self.conv5(out)
+        out = self.act5(out)
+        out = self.norm5(out)
+        out = self.pool5(out)
+        out = self.drop5(out)
+        # print(f"shape of out after layer 5: {out.shape}")
+
+        out = out.view(batch_size, -1)
+        # print(f"shape of out after view: {out.shape}")
 
         out = self.linear6(out)
-        out = torch.squeeze(out)
-        # print(out.shape)
-        # print()
-        # print()
-        # print()
-        # print()
+        # print(f"shape of out after layer 6: {out.shape}")
+        # exit()
+
+        return out
+
+
+class bci_cnn_eeg_first(nn.Module):
+    """
+    Definition of the convolutional neural network used for SSVEP-BCI systems.
+
+    Loss function at page 187!!!
+    """
+
+    def __init__(self, input_shape, input_files, output_files):
+        """ Convolutional Neural Network definition."""
+        super().__init__()
+
+
+        # filter bank
+        self.conv1 = nn.Conv2d(9, 20, kernel_size=1, padding="same").double()
+        self.act1 = nn.ReLU()
+        # self.norm1 = nn.BatchNorm2d(20).double()
+        self.dropout1 = nn.Dropout(p=0.6)
+
+
+        self.conv2 = nn.Conv2d(20, 20, kernel_size=(5, 3),
+                               padding="same").double()
+        self.act2 = nn.ReLU()
+        # self.norm2 = nn.BatchNorm2d(15).double()
+        self.dropout2 = nn.Dropout(p=0.6)
+        self.pool2 = nn.MaxPool2d(kernel_size= 2)
+
+
+        self.conv3 = nn.Conv2d(20, 50, kernel_size=(3, 3),
+                               padding="same").double()
+        self.act3 = nn.ReLU()
+        # self.norm3 = nn.BatchNorm2d(50).double()
+        self.pool3 = nn.MaxPool2d(kernel_size=2)
+        self.dropout3 = nn.Dropout(p=0.6)
+
+
+        self.conv4 = nn.Conv2d(50, 50, kernel_size=1, padding="same").double()
+        self.act4 = nn.ReLU()
+        self.norm4 = nn.BatchNorm2d(50).double()
+        self.dropout4 = nn.Dropout(p=0.6)
+
+
+        self.linear5 = nn.Linear(3100, 40).double()
+
+    def forward(self, x):
+        """Convolutional Neural Network forward pass."""
+        batch_size = x.shape[0]
+
+        # filterbank
+        out = self.conv1(x)
+        out = self.act1(out)
+        out = self.dropout1(out)
+        # print(f"shape of output after layer 1: {out.shape}")
+
+
+        # second stage of filter bank
+        out = self.conv2(out)
+        out = self.act2(out)
+        # out = self.norm2(out)
+        out = self.pool2(out)
+        out = self.dropout2(out)
+        # print(f"shape of output after layer 2: {out.shape}")
+
+
+        # channel selection
+        out = self.conv3(out)
+        out = self.act3(out)
+        # out = self.norm3(out)
+        out = self.pool3(out)
+        out = self.dropout3(out)
+        # print(f"shape of out after layer 3: {out.shape}")
+
+
+        out = self.conv4(out)
+        out = self.act4(out)
+        out = self.norm4(out)
+        out = self.dropout4(out)
+        # print(f"shape of out after layer 4: {out.shape}")
+
+
+        out = out.view(batch_size, -1)
+        # print(f"shape of out after view: {out.shape}")
+
+        out = self.linear5(out)
+        # print(f"shape of out after layer 6: {out.shape}")
+        # exit()
         # exit()
 
         return out
 
 
 class beta_dataset(torch.utils.data.Dataset):
-    """Create a dataset from the BETA database for SSVEP BCI."""
+    """
+    Create a dataset from the BETA database for SSVEP BCI.
+    """
 
-    def __init__(self, eeg_signals, labels):
+    def __init__(self, input_shape, input_files, output_files, num_blocks,
+                 num_chars, eeg_first=False):
         """
-        Parameters.
+        Parameters
         ----------
-        signals : np.array
-            array containing all signal information. With shape:
-                - channels: the number of EEG channels
-                - samples: a sample in each timestep
-                - num of filters: number of bandpass filters applied
-                - number of tests in bci. Ideally, there is a pattern per
-                character, per block, per subject. At least for training
-        labels : np.array
-            array containing all labels for each pattern in 'signals'. With
-            shape:
-            - number of tests in bci (same as 4th dimension in signals)
+        input_shape : tuple
+            len of input shape is the number of dimensions. Each element value
+            corresponds to the cardinality of that dimension
+        input_files : list
+            list of input files to be loaded with np.load
+        output_files : list
+            list of output (labels) files to be loaded with np.load
+        num_blocks : int
+            number of blocks performed in each test
+        num_chars : int
+            number of characters in screen keyboard
+        eef_first : boo
         """
-        if isinstance(eeg_signals, np.ndarray):
-            self.eeg_signals = torch.from_numpy(eeg_signals)
-        elif isinstance(eeg_signals, list):
-            self.eeg_signals = torch.tensor(eeg_signals)
-        else:
-            self.eeg_signals = torch.tensor(eeg_signals)
+        num_subjects = len(input_files)
+        _ds_ = np.zeros((input_shape[0], input_shape[1], input_shape[2],
+                         num_chars * num_blocks * num_subjects))
+        _labels_ = np.zeros(num_chars * num_blocks * num_subjects)
+        zip_files = zip(input_files, output_files)
 
-        if isinstance(labels, np.ndarray):
-            self.labels = torch.from_numpy(labels)
-        elif isinstance(labels, list):
-            self.labels = torch.tensor(labels)
-        else:
-            self.labels = torch.tensor(labels)
+        for subject, (_input_, _output_) in enumerate(zip_files):
+            input = np.load(_input_)
+            output = np.load(_output_)
+            for blck in range(num_blocks):
+                for char in range(num_chars):
+                    _ds_[:, :, :, subject * num_blocks * num_chars
+                        + blck * num_chars + char] = \
+                        input[:, :, :, char, blck]
+                    _labels_[subject * num_blocks * num_chars
+                            + blck * num_chars + char] = \
+                        output[char, blck]
+            del(input)
+            del(output)
+
+        if eeg_first:
+            _ds_ = np.transpose(_ds_, axes=[1, 0, 2, 3])
+
+        self.eeg_signals = torch.from_numpy(_ds_)
+        self.labels = torch.from_numpy(_labels_)
+
 
     def __len__(self):
         """Return number of patterns in dataset."""
---
+++ src/main_eeg_first.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+"""
+Main program after preprocessing of signals.
+
+This program executes after preprocessing.
+It expects to open '*.npy' binary files for a given subject from the BETA
+database. It reads, per subject, an EEG file and a labels file.
+
+The code must check for number of channels, since this is variable depending on
+the configuration being tested.
+
+
+Developer: Marco Antonio Cardoso Moreno (mcardosom2021@cic.ipn.mx
+                                         marcoacardosom@gmail.com)
+
+
+[1] B. Liu, X. Huang, Y. Wang, X. Chen, and X. Gao, "Beta: A large
+        benchmark database toward ssvep-bci application", Frontiers in
+        Neuroscience, vol. 14, p. 627, 2020.
+"""
+
+import os
+import random
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+
+import bci
+
+np.set_printoptions(3)
+torch.set_printoptions(precision=3)
+# torch.manual_seed(7)
+
+"""
+GLOBAL VARIABLES
+"""
+TOTAL_SUBJECTS = 70  # only testing right now for first 15 subjects
+NUM_BLOCKS = 4
+NUM_CHARS = 40  # number of symbols in screen keyboard
+NUM_EPOCHS = 100
+NUM_EPOCHS = 150
+
+SUBJECTS_FOR_TRAIN = 4
+SUBJECTS_FOR_VAL = 1
+
+BATCH_SIZE = 32
+BATCH_SIZE = 16
+
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f"Device where pytorch will execute: {DEVICE}")
+
+"""
+Main code
+"""
+ELECTRODE_CONFIGURATION = "occ"
+file_suffix = f"_{ELECTRODE_CONFIGURATION}.npy"
+data_location = os.path.join("..", "BETA_database",
+                             f"BETA_{ELECTRODE_CONFIGURATION}")
+
+"""
+TRAINING DATASET
+"""
+# input shape: (5, 9, 250, 40, 4)
+# First dimension in dataset, is second in npy file
+# Second dimension in dataset, is first in npy file
+# npy file: (num_filters, num_eeg_channels, sample_points, chars, blocks)
+input_shape = (5, 9, 250, 40, 4)
+
+# dataset:
+#        (num_eeg_channels, num_filters, sample_points, chars *  blocks * users)
+# (9, 5, 250, 40 *  4 * SUBJECTS_FOR_VAL)
+# axis reordering at the end of reading and storing npy file
+train_inputs = []
+train_outputs = []
+for subject in range(1, SUBJECTS_FOR_TRAIN + 1):
+    train_inputs.append(os.path.join(data_location,
+                                             f"S{subject}_input{file_suffix}"))
+    train_outputs.append(os.path.join(data_location,
+                                             f"S{subject}_output{file_suffix}"))
+
+print("Input files")
+for input_file in train_inputs:
+    print(input_file)
+
+train_ds = bci.beta_dataset(input_shape, train_inputs, train_outputs,
+                            NUM_BLOCKS, NUM_CHARS, eeg_first=True)
+train_dl = torch.utils.data.DataLoader(train_ds, batch_size=BATCH_SIZE,
+                                       shuffle=True)
+
+"""
+VALIDATION DATASET
+"""
+val_inputs = []
+val_outputs = []
+for subject in range(SUBJECTS_FOR_TRAIN + 1,
+               SUBJECTS_FOR_TRAIN + SUBJECTS_FOR_VAL + 1):
+    val_inputs.append(os.path.join(data_location,
+                                   f"S{subject}_input{file_suffix}"))
+    val_outputs.append(os.path.join(data_location,
+                                            f"S{subject}_output{file_suffix}"))
+
+_ds_ = np.zeros((input_shape[0], input_shape[1], input_shape[2],
+                 NUM_CHARS * NUM_BLOCKS * SUBJECTS_FOR_VAL))
+_labels_ = np.zeros(NUM_CHARS * NUM_BLOCKS * SUBJECTS_FOR_TRAIN)
+
+val_ds = bci.beta_dataset(input_shape, val_inputs, val_outputs,
+                            NUM_BLOCKS, NUM_CHARS, eeg_first=True)
+val_dl = torch.utils.data.DataLoader(val_ds, batch_size=BATCH_SIZE,
+                                       shuffle=True)
+
+"""
+NEURAL NETWORK
+"""
+bci_net = bci.bci_cnn_eeg_first().to(device=DEVICE)
+
+optimizer = torch.optim.Adam(bci_net.parameters(), lr=0.001)
+loss_fn = torch.nn.CrossEntropyLoss()
+
+bci.training_loop(NUM_EPOCHS, optimizer, bci_net, loss_fn, train_dl,
+                  val_dl, DEVICE)
